737036571de1372a3dfe4e290405eeb327046c8d
nikelui <nikelui@hotmail.it>
2018-06-15 13:27:55 +0200

    Added working Google map

--- app/src/main/java/nike/mapnotes/MainActivity.java
+++ app/src/main/java/nike/mapnotes/MainActivity.java
@@ -1,13 +1,37 @@
 package nike.mapnotes;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap gMap;
+    private LatLng startPoint = new LatLng(43.2,12.5);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+        gMap.addMarker(new MarkerOptions().position(startPoint).title("Hello World!"));
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(startPoint));
+        gMap.moveCamera(CameraUpdateFactory.zoomTo(7.5f));
     }
 }
+
